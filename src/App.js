35d2275aac8ccb6b77863aca569adf7e802b8967
ameny <ouelhazi.amani19@gmail.com>
2022-11-05 17:01:08 +0100

    commit2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@
 import './App.css';
 import './Components/Navbar.js'
 import Navbar from './Components/Navbar.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function App() {
   return (
     <div>
-    <p> salut app</p>
   <Navbar/>
+ 
+
       
     </div>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,27 @@
 
+import Nav from 'react-bootstrap/Nav';
+import './navbar.css';
 
 function Navbar() {
   return (
-    <div>
-       
-      <a href='www.google.tn'>salut nav bar </a>
-      
+    <div className="nav">
+
+    <Nav
+      activeKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item>    
+        <Nav.Link href="/home">Acceuil</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">PikPAk Box</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+    
+    </Nav>
+   
     </div>
   );
 }
