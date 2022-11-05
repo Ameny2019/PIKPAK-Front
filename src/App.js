fdb869c6ef8e032f6953c15125b6dcefd1639b83
ameny <ouelhazi.amani19@gmail.com>
2022-11-05 12:55:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import './Components/Navbar.js'
+import Navbar from './Components/Navbar.js';
+
+function App() {
+  return (
+    <div>
+    <p> salut app</p>
+  <Navbar/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+
+
+function Navbar() {
+  return (
+    <div>
+       
+      <a href='www.google.tn'>salut nav bar </a>
+      
+    </div>
+  );
+}
+
+export default Navbar ;
